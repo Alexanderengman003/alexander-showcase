25fed8b4805e1543997f2aff2706c5b803c459c4
Alexanderengman003 <115240067+Alexanderengman003@users.noreply.github.com>
2025-09-15 20:22:22 +0200

    Update Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -43,7 +43,7 @@ export function Hero() {
               
                 <div className="relative space-y-2">
                   <h2 className="text-lg sm:text-xl lg:text-2xl font-medium text-muted-foreground">
-                    Semiconductor Engineering | Technical Sales
+                    Semiconductor Engineering | Sales
                   </h2>
                   <div className="flex items-center gap-2 text-lg text-muted-foreground">
                     <MapPin className="h-5 w-5" />
